41fb1d40d6b126cfc340345aeba8a4752f931439
Jazz-E <dead__or__alive@hotmail.com>
2017-03-17 06:56:26 +0000

    Update Missed Files. 17/03.

---
+++ PulseEngine2/Component/Collision/MapCollision.cs
@@ -0,0 +1,70 @@
+﻿using PulseEngine.Component.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using PulseEngine.Objects.Sprite;
+
+namespace PulseEngine.Component.Collision
+{
+    public class MapCollision : IEntityComponent, IEntityUpdateComponent
+    {
+        protected Entity _parent;
+
+        List<Entity> _objects
+            = new List<Entity>();
+
+        public List<Entity> objectList
+        {
+            set { this._objects = value; }
+            get { return _objects; }
+        } 
+
+        public void AttachTo(Entity entity)
+        {
+            this._parent = entity;
+        }
+
+        public Entity GetOwner()
+        {
+            return this._parent;
+        }
+
+        public void Initialise()
+        {
+            //Do Nothing
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            BoundingRectangle br = 
+                new BoundingRectangle();
+
+            foreach(IEntityUpdateComponent entity in this._parent.UpdateComponents)
+            {
+                if (entity is BoundingRectangle)
+                {
+                    br = ((BoundingRectangle)entity);
+                    break;
+                }
+            }
+
+            foreach (Entity e in _objects)
+            {
+                
+                
+                foreach(IEntityUpdateComponent entity in e.UpdateComponents)
+                {
+                    if (entity is BoundingRectangle)
+                    {
+                        ((BoundingRectangle)entity).Update(gameTime);
+
+                       if(((BoundingRectangle)entity).Box.Intersects(br.Box))
+                            System.Diagnostics.Debug.Write("Worked!" + e.AssetName + "\n");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PulseEngine2/Display/World/TileMap.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PulseEngine.Objects.Sprite;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using PulseEngine.Component.Collision;
+using Microsoft.Xna.Framework;
+
+namespace PulseEngine.Display.World
+{
+    public class TileMap
+    {
+        public List<Entity> Tiles =
+            new List<Entity>();
+
+        public List<string> TileName =
+            new List<string>();
+
+
+        //Properties 
+        public int MapWidth { get; set; }
+        public int MapHeight { get; set; }
+        public int TileWidth { get; set; }
+        public int TileHeight { get; set; }
+        public int Offset_X { get; set; }
+        public int Offset_Y { get; set; }
+
+        public TileMap()
+        {
+            TileName.Add("_Blank_");
+        }
+
+        public void Load(ContentManager content, 
+           int [,] map, 
+           int tileWidth,
+           int tileHeight, float scale)
+        {
+            MapHeight = map.GetLength(0); //Get first range - Rows
+            MapWidth = map.GetLength(1); //Get Second range - Columns
+
+            TileWidth = tileWidth;
+            TileHeight = tileHeight;
+
+            //Load Map and Tiles into Memory
+            for(int y = 0; y < MapHeight; y++)
+                for(int x = 0; x < MapWidth; x++)
+                {
+                    Tiles.Add(LoadTiles(content, map[y, x], scale));
+                }
+        }
+
+        Entity LoadTiles(ContentManager content, int index, float scale)
+        {
+        BoundingRectangle _boundingBox =
+            new BoundingRectangle();
+
+        Entity _tile;
+            _tile = new Entity();
+
+            _tile.Scale = scale;
+
+            _boundingBox.AttachTo(_tile);
+            _tile.AddComponet(_boundingBox);
+            
+            if (index > 0 && index<TileName.Count)
+            {
+                _tile.AssetName = TileName[index];
+                _tile.Load(content);
+                _tile.Width = TileWidth;
+                _tile.Height = TileHeight;
+            }
+            else
+            {
+                _tile.AssetName = TileName[index];
+                _tile.Width = TileWidth;
+                _tile.Height = TileHeight;
+            }
+            
+            return _tile;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            foreach(Entity e in Tiles)
+            {
+                e.Update(gameTime);
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            int _index = 0;
+
+            if(Tiles.Count>0)
+            foreach(Entity t in Tiles)
+            {
+                int x = _index % MapWidth;
+                int y = _index / MapWidth;
+
+                t.X = x * TileWidth + Offset_X;
+                t.Y = y * TileHeight + Offset_Y;
+
+                    if (t.AssetName != null)
+                    {
+                        if (!t.AssetName.Contains("_Blank_"))
+                        {
+                            t.Draw(spriteBatch);
+                        }_index++;   
+                    }
+
+                   // _index++;
+                }
+
+        }
+    }
+}
